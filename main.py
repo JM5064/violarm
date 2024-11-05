51584aee34cfb83033afe35bf2f98100d1766678
Justin <thisisjustinmao@gmail.com>
2024-11-05 15:34:12 -0500

    Add cropping and helper methods

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
 import numpy as np
+import random
 
 
 mp_hands = mp.solutions.hands
@@ -8,6 +9,7 @@ hands = mp_hands.Hands(min_detection_confidence=0.7, min_tracking_confidence=0.7
 mp_draw = mp.solutions.drawing_utils
 
 cap = cv2.VideoCapture(0)
+
 
 
 def get_still_image():
@@ -21,6 +23,10 @@ def get_still_image():
 
     return frame
 
+
+still_image = get_still_image()
+width = still_image.shape[1]
+height = still_image.shape[0]
 
 
 def prompt_selection(frame, message):
@@ -80,6 +86,30 @@ def prompt_selection(frame, message):
                 return start_point, end_point
             
 
+def display_contours(image):
+    grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    edges = cv2.Canny(grayscale_image, 50, 150)
+
+    contours, _ = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+    image_copy = image.copy()
+
+    for contour in contours:
+        color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        cv2.drawContours(image_copy, [contour], -1, color, 2)
+
+    display_image(image_copy, "Contours")
+
+    return image_copy
+
+
+def display_image(image, window_message):
+    cv2.imshow(window_message, image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows
+            
+
 def get_keypoints(grayscale_image):
     detector = cv2.SIFT_create()
     keypoints, descriptors = detector.detectAndCompute(grayscale_image, None)
@@ -99,28 +129,53 @@ def get_matches(descriptors1, descriptors2, threshold):
 def draw_matches(grayscale_image1, grayscale_image2, keypoints1, keypoints2, matches):
     matched_image = cv2.drawMatches(grayscale_image1, keypoints1, grayscale_image2, keypoints2, matches, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 
-    cv2.imshow("Matches", matched_image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    display_image(matched_image, "Matches")
 
 
-still_image = get_still_image()
+def crop_image(image, top_left_point, bottom_right_point, padding):
+    x1, y1 = top_left_point
+    x2, y2 = bottom_right_point
+
+    x1 -= padding
+    y1 -= padding
+
+    x2 += padding
+    y2 += padding
+
+    if x1 < 0:
+        x1 = 0
+    if y1 < 0:
+        y1 = 0
+    if x2 > width:
+        x2 = width
+    if y2 > height:
+        y2 = height
+
+    return image[y1:y2, x1:x2]
+
+
 grayscale_still_image = cv2.cvtColor(still_image, cv2.COLOR_RGB2GRAY)
+
 still_image_copy = still_image.copy()
 wrist_start, wrist_end = prompt_selection(still_image_copy, "Draw Selection Around Wrist")
 
-x1, y1 = wrist_start
-x2, y2 = wrist_end
+wrist_crop = crop_image(grayscale_still_image, wrist_start, wrist_end, 0)
 
-wrist_crop = grayscale_still_image[y1:y2, x1:x2]
 
-keypoints1, descriptors1 = get_keypoints(grayscale_still_image)
+# get second image
+still_image2 = get_still_image()
+grayscale_still_image2 = cv2.cvtColor(still_image2, cv2.COLOR_RGB2GRAY)
+
+# crop from second image
+wrist_crop_larger = crop_image(grayscale_still_image2, wrist_start, wrist_end, 150)
+
+# match first crop to second crop
+keypoints1, descriptors1 = get_keypoints(wrist_crop_larger)
 keypoints2, descriptors2 = get_keypoints(wrist_crop)
 
-matches = get_matches(descriptors1, descriptors2, 100)
+matches = get_matches(descriptors1, descriptors2, 200)
 
-draw_matches(grayscale_still_image, wrist_crop, keypoints1, keypoints2, matches)
-
+draw_matches(wrist_crop_larger, wrist_crop, keypoints1, keypoints2, matches)
 
 
 cap.release()
