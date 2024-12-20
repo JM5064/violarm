428c2f77158b3450fa6f01b7bd0301d394c7b567
Justin <thisisjustinmao@gmail.com>
2024-12-20 14:17:46 -0500

    Add skip_image and correct_labels

--- label_data.py
+++ label_data.py
@@ -5,6 +5,7 @@ import os
 image_folder = 'images_unlabeled'
 image_paths = [os.path.join(image_folder, f) for f in os.listdir(image_folder) if f.endswith(('.jpg', '.png', '.jpeg'))]
 result_folder = 'images_labeled'
+skip_folder = 'images_skipped'
 csv_file = 'data.csv'
 
 keypoints = {
@@ -38,7 +39,10 @@ def click_event(event, x, y, flags, param):
         points.append([x, y])
 
         if len(points) == 8:
-            save_points()
+            if not correct_labels():
+                points = []
+            else:
+                save_points()
 
     elif event == cv2.EVENT_RBUTTONDOWN:
         # label as not visible
@@ -48,15 +52,17 @@ def click_event(event, x, y, flags, param):
         points.append([-1, -1])
 
         if len(points) == 8:
-            save_points()
+            if not correct_labels():
+                points = []
+            else:
+                save_points()
 
 
 # save points to csv and move image to labeled folder
 def save_points():
     global points, image_path
 
-    image_name = image_path.removeprefix(image_folder)
-    result_path = result_folder + image_name
+    result_path = image_path.replace(image_folder, result_folder)
 
     os.rename(image_path, result_path)
 
@@ -64,7 +70,7 @@ def save_points():
         writer = csv.writer(csvfile)
         writer.writerow([result_path] + [coord for point in points for coord in point])
 
-    print(f"Points saved to {csv_file} for {image_name}: {points}")
+    print(f"Points saved to {csv_file} for {result_path}: {points}")
     
     points = []
 
@@ -84,7 +90,28 @@ def next_image():
         cv2.destroyAllWindows()
 
 
-if image_paths:
+def skip_image():
+    skip_path = image_path.replace(image_folder, skip_folder)
+    os.rename(image_path, skip_path)
+
+
+def correct_labels():
+    global image_path
+
+    # check finger labels
+    if not (points[0][1] <= points[1][1] <= points[2][1] <= points[3][1]):
+        print(f"{image_path} Labeled fingers incorrectly")
+        return False
+    
+    if not (points[4][0] <= points[5][0] and points[5][1] <= points[6][1] and 
+            points[6][0] >= points[7][0] and points[7][1] >= points[4][1]):
+        print(f"{image_path} Labeled arm incorrectly")
+        return False
+
+    return True
+
+
+while image_paths:
     image_path = image_paths.pop(0)
     image = cv2.imread(image_path)
 
@@ -92,7 +119,12 @@ if image_paths:
 
     cv2.setMouseCallback("Image", click_event)
 
-    cv2.waitKey(0)
+    # press space to skip image. otherwise, quit
+    key = cv2.waitKey(0)
+    if key == 32:
+        skip_image()
+    else:
+        break
 
-print("done ")
+print("done")
 cv2.destroyAllWindows()
