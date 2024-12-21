0596724832d254ee2638d518e827d4ab68b92569
Justin <thisisjustinmao@gmail.com>
2024-12-21 18:27:08 -0500

    Add second camera

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import cv2
 import numpy as np
-import mediapipe as mp
 import arm
 import utils
 import time
@@ -8,12 +7,10 @@ import torch
 from torchvision import transforms
 from ultralytics import YOLO
 
-mp_hands = mp.solutions.hands
-hands = mp_hands.Hands(min_detection_confidence=0.7, min_tracking_confidence=0.7)
-mp_draw = mp.solutions.drawing_utils
-
 model = YOLO("runs/pose/yolov11/weights/best.pt")
-cap = cv2.VideoCapture(0)
+main_cap = cv2.VideoCapture(0)
+url = "camera_ip"
+side_cap = cv2.VideoCapture(url)
 
 total_time = 0
 total_frames = 0
@@ -25,34 +22,48 @@ def predict(frame):
     return results
 
 
+def draw_arm_outline(frame, keypoints):
+    points = keypoints.numpy()[0]
+
+    corners = []
+    for point in points:
+        x, y = int(point[0]), int(point[1])
+        corners.append([x, y])
+        cv2.circle(frame, (x, y), 3, (255, 0, 0), 3)
+
+    corners[-1], corners[-2] = corners[-2], corners[-1]
+    corners = np.array(corners)
+    corners = corners.reshape((-1, 1, 2))
+    cv2.polylines(frame, [corners], isClosed=True, color=(255, 0, 0), thickness=2)
+
+    return frame
+
+
 while True:
     start = time.time()
-    ret, frame = cap.read()
 
-    results = predict(frame)
+    if main_cap.isOpened():
+        ret_main, frame_main = main_cap.read()
 
-    keypoints = results[0].keypoints.xy
-    if keypoints.shape == torch.Size([1, 4, 2]):
-        points = keypoints.numpy()[0]
+        results = predict(frame_main)
 
-        corners = []
-        for point in points:
-            x, y = int(point[0]), int(point[1])
-            corners.append([x, y])
-            cv2.circle(frame, (x, y), 3, (255, 0, 0), 3)
+        keypoints = results[0].keypoints.xy
+        if keypoints.shape == (1, 4, 2):
+            frame_main = draw_arm_outline(frame_main, keypoints)
 
-        corners[-1], corners[-2] = corners[-2], corners[-1]
-        corners = np.array(corners)
-        corners = corners.reshape((-1, 1, 2))
-        cv2.polylines(frame, [corners], isClosed=True, color=(255, 0, 0), thickness=2)
+        cv2.imshow("Main Frame", frame_main)
 
-    # hand_points = hands.process(frame)
-    # if hand_points.multi_hand_landmarks:
-    #     for hand_landmarks in hand_points.multi_hand_landmarks:
-    #         mp_draw.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+    if side_cap.isOpened():
+        ret_side, frame_side = side_cap.read()
 
+        results = predict(frame_side)
 
-    cv2.imshow("Frame", frame)
+        keypoints = results[0].keypoints.xy
+        if keypoints.shape == (1, 4, 2):
+            frame_side = draw_arm_outline(frame_side, keypoints)
+            
+        cv2.imshow("Side Frame", frame_side)
+
 
     end = time.time()
     total_time += end - start
@@ -67,6 +78,6 @@ print("Average time per frame: ", average_time, "ms")
 print(1000 / average_time, "fps")
 print("Average additional time per frame: ", average_time - default_time, "ms")
 
-cap.release()
+main_cap.release()
 cv2.destroyAllWindows()
 
