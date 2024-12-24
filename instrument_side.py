6a1dbba4d9a12ca99feae91b997b7f1800f83a90
Justin <thisisjustinmao@gmail.com>
2024-12-24 14:36:27 -0500

    Convert instrument_side to class

--- instrument_side.py
+++ instrument_side.py
@@ -2,38 +2,43 @@ import numpy as np
 
 # TODO: time these methods to see whether class implementation is faster
 
-
-def get_sideline(top_right, bottom_right):
-    a = top_right[1] - bottom_right[1]
-    b = bottom_right[0] - top_right[0]
-
-    if a == b == 0:
-        return None
-    
-    c = -(a * top_right[0] + b * top_right[1])
-
-    return a, b, c
+class InstrumentSide:
+    def __init__(self, keypoints):
+        self.top_right = keypoints[1]
+        self.bottom_right = keypoints[2]
 
 
-def get_pressed_fingers(fingers, top_right, bottom_right):
-    return [finger for finger in fingers if is_pressed(finger, top_right, bottom_right, 50)]
+    def get_sideline(self):
+        a = self.top_right[1] - self.bottom_right[1]
+        b = self.bottom_right[0] - self.top_right[0]
+
+        if a == b == 0:
+            return None
+        
+        c = -(a * self.top_right[0] + b * self.top_right[1])
+
+        return a, b, c
+
+
+    def get_pressed_fingers(self, fingers):
+        return [finger for finger in fingers if self.is_pressed(finger, 50)]
 
 
 
-def is_pressed(finger, top_right, bottom_right, threshold):
-    x, y = finger
-    sideline = get_sideline(top_right, bottom_right)
+    def is_pressed(self, finger, threshold):
+        x, y = finger
+        sideline = self.get_sideline()
 
-    if sideline is None:
-        # assume vertical line
-        dist = abs(x - top_right[0])
-    else:
-        a, b, c = sideline
+        if sideline is None:
+            # assume vertical line
+            dist = abs(x - self.top_right[0])
+        else:
+            a, b, c = sideline
 
-        dist = abs(a * x + b * y + c) / np.sqrt(a**2 + b**2)
+            dist = abs(a * x + b * y + c) / np.sqrt(a**2 + b**2)
 
-    
-    return dist <= threshold
+        
+        return dist <= threshold
 
 
 
