5be62e1b0217f3c492a720122d52a6f9737f7a7f
Justin <thisisjustinmao@gmail.com>
2024-12-25 17:17:03 -0500

    Implement get_notes

--- instrument/instrument_front.py
+++ instrument/instrument_front.py
@@ -1,9 +1,8 @@
 from instrument_arm import InstrumentArm
 import numpy as np
 import time
-import cv2
 
-class InstrumentFront:
+class InstrumentFront(InstrumentArm):
 
     def __init__(self, keypoints):
         self.top_left = keypoints[0]
@@ -16,41 +15,66 @@ class InstrumentFront:
         if num_strings <= 0:
             raise Exception("num_strings must be greater than 0")
         
-        top_points = self.divide_baseline(top_left, top_right, num_strings)
-        bottom_points = self.divide_baseline(bottom_left, bottom_right, num_strings)
+        top_points = self.divide_baseline(self.top_left, self.top_right, num_strings)
+        bottom_points = self.divide_baseline(self.bottom_left, self.bottom_right, num_strings)
+        
+        notes = []
+        strings = []
+        for finger in fingers:
+            if not self.in_quadrilateral(finger, self.top_left, self.top_right, self.bottom_right, self.bottom_left):
+                continue
 
+            min_distance = float('inf')
+            for i in range(num_strings):
+                distance = self.distance_to_line(finger, top_points[i], bottom_points[i])
 
+                if distance < min_distance:
+                    min_distance = distance
+                    closest_string = i
+                else:
+                    break
+            
+            strings.append(closest_string)
+            notes.append(self.get_note_fraction(finger, top_points[closest_string], bottom_points[closest_string]))
+            
+        return strings, notes
+        
 
     def divide_baseline(self, p1, p2, num_strings):
-        points = [self.top_left]
-        dx = abs(p2[0] - p1[0]) / num_strings
-        dy = abs(p2[1] - p1[1]) / num_strings
-        for i in range(1, num_strings):
+        points = []
+
+        dx = abs(p2[0] - p1[0]) / (num_strings * 2)
+        dy = abs(p2[1] - p1[1]) / (num_strings * 2)
+
+        for i in range(1, num_strings * 2, 2):
             x = p1[0] + i * dx
             y = p1[1] + i * dy
 
             points.append([x, y])
 
-        points.append(top_right)
-
         return points
+    
+
+    def get_note_fraction(self, p, line_p1, line_p2):
+        projected_vector = self.get_projection_vector(p, line_p1, line_p2)
+
+        return np.linalg.norm(projected_vector) / self.distance(line_p1, line_p2)
 
 
 
-top_left = [444,238]
-top_right = [531,250]
-bottom_right = [511,617]
-bottom_left = [397,623]
+top_left = [0, 0]
+top_right = [1200,0]
+bottom_right = [1600,1200]
+bottom_left = [0,1200]
 
 keypoints = [top_left, top_right, bottom_right, bottom_left]
-pressed_fingers = [[432,322], [402,394]]
+pressed_fingers = [[432,322], [80, 1000]]
 
 start = time.time()
+instrumentFront = InstrumentFront(keypoints)
 
-for i in range(1):
-    instrumentFront = InstrumentFront(keypoints)
-
-    instrumentFront.get_string_lines(pressed_fingers, 4)
+for i in range(10000):
+    instrumentFront.get_notes(pressed_fingers, 4)
 
 end = time.time()
 
