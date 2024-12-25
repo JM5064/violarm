7f01a46eaceef9e8eb82770186842b5b2146a528
Justin <thisisjustinmao@gmail.com>
2024-12-25 16:02:36 -0500

    Switch instrument_arm to use linalg

--- instrument/instrument_arm.py
+++ instrument/instrument_arm.py
@@ -1,31 +1,57 @@
 from abc import ABC
 import numpy as np
+import time
 
 class InstrumentArm(ABC):
 
     def distance(self, p1, p2):
         return np.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
+            
+    
+    def get_projection_vector(self, p, line_p1, line_p2):
+        """Projects vector line_p1-p onto line_p1-line_p2
+        args:
+            p: list[int, int]
+            line_p1: list[int, int]
+            line_p2: list[int, int]
+        
+        returns: 
+            projected vector: list[int, int]
+        """
+
+        line_vector = [line_p2[0] - line_p1[0], line_p2[1] - line_p1[1]]
+        to_point_vector = [p[0] - line_p1[0], p[1] - line_p1[1]]
+
+        line_vector_norm = np.linalg.norm(line_vector)
+        if line_vector_norm == 0:
+            raise Exception("Invalid line points")
+        
+        lhs = self.dot2d(line_vector, to_point_vector) / (line_vector_norm ** 2)
+
+        return [lhs * line_vector[0], lhs * line_vector[1]]
     
 
-    def distance_to_line(self, p, line):
-        x, y = p
+    def get_closest_point_to_line(self, p, line_p1, line_p2):
+        projected_vector = self.get_projection_vector(p, line_p1, line_p2)
 
-        if line is None:
-            # assume vertical line
-            return abs(x - self.top_right[0])
-        else:
-            a, b, c = line
-            
-            return abs(a * x + b * y + c) / np.sqrt(a**2 + b**2)
+        return [line_p1[0] + projected_vector[0], line_p1[1] + projected_vector[1]]
+    
+    
+    def distance_to_line(self, p, line_p1, line_p2):
+        point_on_line = self.get_closest_point_to_line(p, line_p1, line_p2)
+
+        return self.distance(p, point_on_line)
 
 
-    def get_line(self, p1, p2):
-        a = p1[1] - p2[1]
-        b = p2[0] - p1[0]
+    def get_note_fraction(self, p, line_p1, line_p2):
+        projected_vector = self.get_projection_vector(p, line_p1, line_p2)
 
-        if a == b == 0:
-            return None
-        
-        c = -(a * p1[0] + b * p1[1])
+        return np.linalg.norm(projected_vector) / self.distance(line_p1, line_p2)
 
-        return a, b, c
+
+    def dot2d(self, p1, p2):
+        return p1[0] * p2[0] + p1[1] * p2[1]
+    
+
+    def in_bounds(self, p):
+        pass
