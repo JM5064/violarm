45e2124ece889e2f2f51912167925738e5236c0d
Justin <thisisjustinmao@gmail.com>
2024-12-25 17:36:41 -0500

    Add distance_threshold parameter

--- instrument/instrument_side.py
+++ instrument/instrument_side.py
@@ -4,13 +4,14 @@ import numpy as np
 # TODO: time these methods to see whether class implementation is faster
 
 class InstrumentSide(InstrumentArm):
-    def __init__(self, keypoints):
+    def __init__(self, keypoints, distance_threshold):
         self.top_right = keypoints[1]
         self.bottom_right = keypoints[2]
+        self.distance_threshold = distance_threshold
 
 
     def get_pressed_fingers(self, fingers):
-        return [finger for finger in fingers if self.is_pressed(finger, 50)]
+        return [finger for finger in fingers if self.is_pressed(finger, self.distance_threshold)]
 
 
     def is_pressed(self, finger, threshold):
