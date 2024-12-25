1ef8f2746a8caef03f1e258637be2781f022f3cf
Justin <thisisjustinmao@gmail.com>
2024-12-25 17:16:30 -0500

    Implement in_quadrilateral

--- instrument/instrument_arm.py
+++ instrument/instrument_arm.py
@@ -1,6 +1,5 @@
 from abc import ABC
 import numpy as np
-import time
 
 class InstrumentArm(ABC):
 
@@ -43,15 +42,20 @@ class InstrumentArm(ABC):
         return self.distance(p, point_on_line)
 
 
-    def get_note_fraction(self, p, line_p1, line_p2):
-        projected_vector = self.get_projection_vector(p, line_p1, line_p2)
-
-        return np.linalg.norm(projected_vector) / self.distance(line_p1, line_p2)
-
-
     def dot2d(self, p1, p2):
         return p1[0] * p2[0] + p1[1] * p2[1]
     
 
-    def in_bounds(self, p):
-        pass
+    def in_quadrilateral(self, p, a, b, c, d):
+
+        def triangle_area(a, b, c):
+            u = [b[0] - a[0], b[1] - a[1]]
+            v = [c[0] - b[0], c[1] - b[1]]
+
+            return 0.5 * abs(u[0] * v[1] - u[1] * v[0])
+        
+        quadrilateral_area = triangle_area(a, b, c) + triangle_area(a, d, c)
+        point_triangle_sum = triangle_area(a, b, p) + triangle_area(b, p, c) + triangle_area(c, p, d) + triangle_area(d, p, a)
+
+        return abs(quadrilateral_area - point_triangle_sum) < 0.1
+
