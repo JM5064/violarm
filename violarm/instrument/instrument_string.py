82b4d9e84aa21fcd22cc14d9d202558d1580b6ba
Justin <thisisjustinmao@gmail.com>
2025-01-23 10:17:32 -0500

    Add to_frequency

--- violarm/instrument/instrument_string.py
+++ violarm/instrument/instrument_string.py
@@ -1,9 +1,29 @@
 
 class InstrumentString:
     
-    def __init__(self, min_pitch, max_pitch, equal_tuning=False):
-        self.min_pitch = min_pitch
-        self.max_pitch = max_pitch
+    def __init__(self, min_freq, max_freq=2637, string_length=0.325, equal_tuning=False):
+        self.min_freq = min_freq
+        self.max_freq = max_freq
+
+        self.string_length = string_length
+        self.velocity = min_freq * 2 * string_length
 
         self.equal_tuning = equal_tuning
 
+
+    def to_frequency(self, note_fraction):
+        if self.equal_tuning:
+            return round(self.min_freq + note_fraction * (self.max_freq - self.min_freq))
+        
+        new_length = self.string_length - self.string_length * note_fraction
+        if new_length == 0:
+            return self.max_freq
+        
+        new_frequency = round(self.velocity / (2 * new_length))
+
+        if new_frequency > self.max_freq:
+            return self.max_freq
+
+        return new_frequency
+
+        
