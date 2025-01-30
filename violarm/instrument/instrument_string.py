fd6c9adb69c626a664f5d3b64e3c60016e1717cc
Justin <thisisjustinmao@gmail.com>
2025-01-30 17:24:03 -0500

    Add to_fraction

--- violarm/instrument/instrument_string.py
+++ violarm/instrument/instrument_string.py
@@ -4,6 +4,8 @@ class InstrumentString:
     def __init__(self, min_freq: int, max_freq=2637, string_length=0.325, equal_tuning=False):
         self.min_freq = min_freq
         self.max_freq = max_freq
+        if self.max_freq <= self.min_freq:
+            self.max_freq = self.min_freq + 1
 
         self.string_length = string_length
         self.velocity = min_freq * 2 * string_length
@@ -43,6 +45,7 @@ class InstrumentString:
         if new_length == 0:
             return self.max_freq
         
+        # Standing wave equation
         frequency = round(self.velocity / (2 * new_length))
 
         # Threshold frequency to make sure it's not too high
@@ -52,3 +55,27 @@ class InstrumentString:
         return frequency
 
         
+    def to_fraction(self, note_frequency: int) -> float:
+        """Converts frequency to fractional note form
+        args:
+            note_frequency: int
+
+        returns:
+            fraction: float
+        """
+
+        if self.equal_tuning:
+            return (note_frequency - self.min_freq) / (self.max_freq - self.min_freq)
+        
+        if 2 * note_frequency * self.string_length == 0:
+            return None #
+        
+        if note_frequency == 0:
+            return 1
+        
+        new_length = self.velocity / (2 * note_frequency)
+        
+        fraction = (self.string_length - new_length) / self.string_length
+
+        return fraction
+
